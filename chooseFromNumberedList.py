1d348c19e0b19d7d8815aa64ceade30e8b08a663
maestraccio <maestraccio@musician.org>
2025-05-17 18:52:14 +0200

    Versie 0.2: Kleine aanpassingen

--- chooseFromNumberedList.py
+++ chooseFromNumberedList.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 
-# Version 0.1
-# Date 20250516
-
-import random
-
+# Version 0.2
+# Date 20250517
+#
 # This module allows you to print and select from items in a given list by
 # entering the number or moving the selector up and down with the plus and mius
-# signs, or with brackets pointing left (back) or right (forward). Confirm every
-# entry with the Enter key.
+# signs, or with brackets pointing left (backward) or right (forward), also
+# multiple in one input. Confirm every input with the Enter key.
 #
 # The function requires exactly four arguments:
 # the list:            the given [list, with, the, items] to choose from
@@ -16,13 +14,15 @@ import random
 # the starting number: common are 0 or 1, but any positive number can be given
 # the default choice:  a number in the numbering list, between the lowest and
 #                      the highest number in the list (inclusive)
+# It returns the selected item in the list and its index number in that list.
 
-nexti = [")","}","]",">","+"]
-previ = ["(","{","[","<","-"]
-this = "-> "
-thisnot = "   "
-connect = " : "
 def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZeroOrOne,DefaultOption):
+    import random
+    nexti = [")","}","]",">","+"]
+    previ = ["(","{","[","<","-"]
+    this = "-> "
+    thisnot = "   "
+    connect = " : "
     Int = DefaultOption
     IntList = []
     if AscendingOrDescendingOrRandom.upper() == "D":
@@ -39,11 +39,11 @@ def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZ
     else:
         for item in NumberedList:
             IntList.append(NumberedList.index(item)+StartWithZeroOrOne)
-    
-    # print arrow+number+connector+listitem
-    # The arrow goes where the targeted number is (DefaultOption)
     def printNumberedList(Int):
-        ValidRange = range(StartWithZeroOrOne,StartWithZeroOrOne+len(NumberedList)-1)
+        StrList = []
+        for i in IntList:
+            StrList.append(str(i))
+        maxlen = len(max(StrList,key = len))
         Index = (Int-StartWithZeroOrOne) % len(IntList)
         if AscendingOrDescendingOrRandom.upper() == "D":
             PrInt = len(IntList)-1-IntList.index(Index+StartWithZeroOrOne)
@@ -51,16 +51,15 @@ def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZ
             PrInt = IntList.index(Index+StartWithZeroOrOne)
         for i in IntList:
             if i-StartWithZeroOrOne == PrInt:
-                print(this+"{:>2}".format(i)+connect+str(NumberedList[IntList.index(i)]))
+                print(this+("{:>%d}" % maxlen).format(i)+connect+str(NumberedList[IntList.index(i)]))
             else:
-                print(thisnot+"{:>2}".format(i)+connect+str(NumberedList[IntList.index(i)]))
+                print(thisnot+("{:>%d}" % maxlen).format(i)+connect+str(NumberedList[IntList.index(i)]))
         return Int
     printNumberedList(Int)
     yes = True
     while yes == True:
         ip = input()
         try:
-            #Int = NumberedList[int(ip) % len(NumberedList)-StartWithZeroOrOne]
             Int = int(ip)
         except(Exception) as e:
             #print(e)
@@ -77,5 +76,5 @@ def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZ
                 else:
                     pass
         printNumberedList(Int)
-    # 0 = item in list, 1 = chosen number (not index!)
-    return NumberedList[IntList.index(Int)],Int
+    # 0 = item in list, 1 = index of that item in list
+    return NumberedList[IntList.index(Int)],Int-StartWithZeroOrOne
