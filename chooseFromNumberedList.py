37813426ddcf2a06f7ead17e4cd41a4fbc6acd40
maestraccio <maestraccio@musician.org>
2025-05-23 23:40:27 +0200

    Versie 0.3: Toegevoegd chooseFromKeysList

--- chooseFromNumberedList.py
+++ chooseFromNumberedList.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-# Version 0.2
-# Date 20250517
+# Version 0.3
+# Date 20250523
 #
 # This module allows you to print and select from items in a given list by
 # entering the number or moving the selector up and down with the plus and mius
@@ -15,7 +15,6 @@
 # the default choice:  a number in the numbering list, between the lowest and
 #                      the highest number in the list (inclusive)
 # It returns the selected item in the list and its index number in that list.
-
 def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZeroOrOne,DefaultOption):
     import random
     nexti = [")","}","]",">","+"]
@@ -60,21 +59,84 @@ def chooseFromNumberedList(NumberedList,AscendingOrDescendingOrRandom,StartWithZ
     while yes == True:
         ip = input()
         try:
-            Int = int(ip)
+            Index = (int(ip) - StartWithZeroOrOne) % len(NumberedList)
+            Int = Index + StartWithZeroOrOne
         except(Exception) as e:
             #print(e)
             if ip == "":
                 break
             for k in ip:
                 if k in nexti:
-                    try:
-                        Int = Int+1
-                    except:
-                        Int = Int
+                    Index = (Int - StartWithZeroOrOne + 1) % len(NumberedList)
+                    Int = Index + StartWithZeroOrOne
                 elif k in previ:
-                    Int = Int-1
+                    Index = (Int - StartWithZeroOrOne - 1) % len(NumberedList)
+                    Int = Index + StartWithZeroOrOne
                 else:
                     pass
         printNumberedList(Int)
     # 0 = item in list, 1 = index of that item in list
     return NumberedList[IntList.index(Int)],Int-StartWithZeroOrOne
+# This module allows you to print and select from items in a given list by
+# entering the key or moving the selector up and down with the plus and mius
+# signs, or with brackets pointing left (backward) or right (forward), also
+# multiple in one input. Confirm every input with the Enter key.
+#
+# The function requires exactly four arguments:
+# the List:           the given [list, with, the, items] to choose from
+# the Keys List:      the list with all possible keys to enter on input. Both
+#                     lists MUST be equal in length
+# the Case:           "U"pper, "l"ower or "C"ase sensitive
+# the Default Choice: The the default option key in the KeysList
+# It returns the selected item in the list and the corresponding key.
+def chooseFromKeysList(NotNumberedList,KeysList,UpperOrLower,DefaultOption):
+    nexti = [")","}","]",">","+"]
+    previ = ["(","{","[","<","-"]
+    this = "-> "
+    thisnot = "   "
+    connect = " : "
+    if UpperOrLower.upper() == "U":
+        Option = DefaultOption.upper()
+    elif UpperOrLower.lower() == "l":
+        Option = DefaultOption.lower()
+    else:
+        Option = DefaultOption
+    if len(NotNumberedList) != len(KeysList):
+        print("len(NumberedList) != len(KeysList)")
+        return
+    def printNotNumberedList(Option):
+        maxlen = len(max(KeysList,key = len))
+        PrOption = KeysList[KeysList.index(Option)]
+        for i in KeysList:
+            if i == PrOption:
+                print(this+("{:>%d}" % maxlen).format(i)+connect+str(NotNumberedList[KeysList.index(i)]))
+            else:
+                print(thisnot+("{:>%d}" % maxlen).format(i)+connect+str(NotNumberedList[KeysList.index(i)]))
+        return Option
+    printNotNumberedList(Option)
+    yes = True
+    while yes == True:
+        if UpperOrLower.upper() == "U":
+            ip = input().upper()
+        elif UpperOrLower.lower() == "l":
+            ip = input().lower()
+        else:
+            ip = input()
+        if ip in KeysList:
+            Option = ip
+        else:
+            if ip == "":
+                break
+            for k in ip:
+                if k in nexti:
+                    try:
+                        Option = KeysList[KeysList.index(Option)+1]
+                    except:
+                        Option = KeysList[0]
+                elif k in previ:
+                    Option = KeysList[KeysList.index(Option)-1]
+                else:
+                    pass
+        printNotNumberedList(Option)
+    # 0 = item in list, 1 = chosen option
+    return NotNumberedList[KeysList.index(Option)],Option
