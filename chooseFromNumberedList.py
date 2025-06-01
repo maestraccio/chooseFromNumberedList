91330a36326c87ec199c8a16ec205a352c3f8df9
maestraccio <maestraccio@musician.org>
2025-06-01 12:04:40 +0200

    Versie 2.00: HiddenList geïntroduceerd

--- chooseFromNumberedList.py
+++ chooseFromNumberedList.py
@@ -1,28 +1,55 @@
 #!/usr/bin/python
 
-# Version 1.07
-# Date 20250531
+# Version 2.00
+# Date 20250601
 
 # **chooseFromNumberedList** allows you to print and select from items in a given
 # list by entering the number or moving the selector up and down with the plus
 # and minus signs, or with brackets pointing left (backward) or right (forward),
 # also multiple in one input. Confirm every input with the Enter key.
-# The function requires one list of four or five elements:
+# The function requires one list of four, five or six elements:
 # the NumberedList:    the given [list, with, the, items] to choose from
 # the Sorting Method:  "A" = Ascending, "D" = Descending, "R" = Random (LOL)
 # the Starting Number: common are 0 or 1, but any positive number can be given
 # the Default Option:  a number in the numbering list, between the lowest and
 #                      the highest number in the list (inclusive)
-# Optional:            a customized indicator: Can contain a colour code (don't
-#                      forget to close it with "\033[0m"!). Default string length
-#                      is 3, but you can also change the length to make it stand
-#                      out. If it is "O", the chosen option is used.
-# It returns the selected item in the list and its index number in that list.
+# Optional1:           a customized indicator: Can contain a colour code
+#                      (don't forget to close it with "\033[0m"!). Default
+#                      string length is 3, but you can also change the length
+#                      to make it stand out. If it is "O", the first three
+#                      characters of the chosen key are used.
+# Optional2:           A list with valid options that are not shown in the
+#                      NumberedList. ATTENTION: It returns only the chosen
+#                      option, twice!
+# It returns the selected value and its key, except when an item from HiddenList
+# is chosen.
 def chooseFromNumberedList(Import):
     NumberedList =                  Import[0]
     AscendingOrDescendingOrRandom = Import[1]
     StartWithZeroOrOne =            Import[2]
     DefaultOption =                 Import[3]
+    try:
+        if type(Import[4]) == str:
+            if Import[4].upper() == "O":
+                this = "{:^3}".format(Key)[:4]
+            else:
+                this = Import[4]
+            try:
+                HiddenList = Import[5]
+            except:
+                HiddenList = []
+        elif type(Import[4]) == list:
+            this = "-> "
+            HiddenList = Import[4]
+        else:
+            this = "-> "
+            try:
+                HiddenList = Import[5]
+            except:
+                HiddenList = []
+    except:
+        this = "-> "
+        HiddenList = []
     nexti = [")","}","]",">","+"]
     previ = ["(","{","[","<","-"]
     thisnot = "   "
@@ -45,13 +72,6 @@ def chooseFromNumberedList(Import):
         for item in NumberedList:
             IntList.append(NumberedList.index(item)+StartWithZeroOrOne)
     def printNumberedList(Int):
-        try:
-            if Import[4].upper() == "O":
-                this = "{:^3}".format(Int)[:3]
-            else:
-                this = str(Import[4])
-        except:
-            this = "-> "
         StrList = []
         for i in IntList:
             StrList.append(str(i))
@@ -76,7 +96,9 @@ def chooseFromNumberedList(Import):
             Int = Index + StartWithZeroOrOne
         except(Exception) as e:
             #print(e)
-            if ip == "":
+            if ip in HiddenList:
+                return ip,ip
+            elif ip == "":
                 break
             for k in ip:
                 if k in nexti:
@@ -95,22 +117,49 @@ def chooseFromNumberedList(Import):
 # by entering the key or moving the selector up and down with the plus and minus
 # signs, or with brackets pointing left (backward) or right (forward), also
 # multiple in one input. Confirm every input with the Enter key.
-# The function requires one list of four or five elements:
+# The function requires one list of four, five or six elements:
 # the NotNumberedList: the given [list, with, the, items] to choose from
 # the Keys List:       the list with all possible keys to enter on input. Both
 #                      lists MUST be equal in length
 # the Case:            "U"pper, "l"ower or "C"ase sensitive
 # the Default Choice:  The the default option key in the KeysList
-# Optional:            a customized indicator: Can contain a colour code (don't
-#                      forget to close it with "\033[0m"!). Default string length
-#                      is 3, but you can also change the length to make it stand
-#                      out. If it is "O", the chosen option is used.
-# It returns the selected item in the list and the corresponding key.
+# Optional1:           a customized indicator: Can contain a colour code
+#                      (don't forget to close it with "\033[0m"!). Default
+#                      string length is 3, but you can also change the length
+#                      to make it stand out. If it is "O", the first three
+#                      characters of the chosen key are used.
+# Optional2:           A list with valid options that are not shown in the
+#                      KeysList. ATTENTION: It returns only the chosen option,
+#                      twice!
+# It returns the selected value and its key, except when an item from HiddenList
+# is chosen.
 def chooseFromKeysList(Import):
     NotNumberedList = Import[0]
     KeysList =        Import[1]
     UpperOrLower =    Import[2]
     DefaultOption =   Import[3]
+    try:
+        if type(Import[4]) == str:
+            if Import[4].upper() == "O":
+                this = "{:^3}".format(Key)[:4]
+            else:
+                this = Import[4]
+            try:
+                HiddenList = Import[5]
+            except:
+                HiddenList = []
+        elif type(Import[4]) == list:
+            this = "-> "
+            HiddenList = Import[4]
+        else:
+            this = "-> "
+            try:
+                HiddenList = Import[5]
+            except:
+                HiddenList = []
+    except:
+        this = "-> "
+        HiddenList = []
     nexti = [")","}","]",">","+"]
     previ = ["(","{","[","<","-"]
     thisnot = "   "
@@ -125,13 +174,6 @@ def chooseFromKeysList(Import):
         print("len(NumberedList) != len(KeysList)")
         return
     def printNotNumberedList(Option):
-        try:
-            if Import[4].upper() == "O":
-                this = "{:^3}".format(Option)[:3]
-            else:
-                this = str(Import[4])
-        except:
-            this = "-> "
         maxlen = len(max(KeysList,key = len))
         PrOption = KeysList[KeysList.index(Option)]
         for i in KeysList:
@@ -149,7 +191,9 @@ def chooseFromKeysList(Import):
             ip = input().lower()
         else:
             ip = input()
-        if ip in KeysList:
+        if ip in HiddenList:
+            return ip,ip
+        elif ip in KeysList:
             Option = ip
         else:
             if ip == "":
@@ -171,42 +215,63 @@ def chooseFromKeysList(Import):
 # **chooseFromList** allows you to print and select from items in a given list by
 # moving the selector up and down with the plus and minus signs, or with brackets
 # pointing left (backward) or right (forward), also multiple in one input.
-# Confirm every input with the Enter key.  The function requires one list of two
-# or three elements:
+# Confirm every input with the Enter key.
+# The function requires one list of two, three or four elements:
 # the List:            the given [list, with, the, items] to choose from
 # the Default Option:  an index number in the List
-# Optional:            a customized indicator: Can contain a colour code (don't
-#                      forget to close it with "\033[0m"!). Default string length
-#                      is 3, but you can also change the length to make it stand
-#                      out. If it is "O", the chosen option is used.
-# It returns the selected item in the list and its index number in that list.
+# Optional1:           a customized indicator: Can contain a colour code
+#                      (don't forget to close it with "\033[0m"!). Default
+#                      string length is 3, but you can also change the length
+#                      to make it stand out. If it is "O", the first three
+#                      characters of the chosen key are used.
+# Optional2:           A list with valid options that are not shown in the
+#                      List. ATTENTION: It returns only the chosen option, twice!
+# It returns the selected value and its key, except when an item from HiddenList
+# is chosen.
 def chooseFromList(Import):
     List = Import[0]
     Int =  Import[1]
+    try:
+        if type(Import[2]) == str:
+            if Import[2].upper() == "O":
+                this = "{:^3}".format(Key)[:2]
+            else:
+                this = Import[2]
+            try:
+                HiddenList = Import[3]
+            except:
+                HiddenList = []
+        elif type(Import[2]) == list:
+            this = "-> "
+            HiddenList = Import[2]
+        else:
+            this = "-> "
+            try:
+                HiddenList = Import[3]
+            except:
+                HiddenList = []
+    except:
+        this = "-> "
+        HiddenList = []
     nexti = [")","}","]",">","+"]
     previ = ["(","{","[","<","-"]
     thisnot = "   "
     connect = " : "
     def printList(Int):
-        try:
-            if Import[2].upper() == "O":
-                this = "{:^3}".format(Int)[:3]
-            else:
-                this = str(Import[2])
-        except:
-            this = "-> "
         Index = Int
         for i in List:
             if List.index(i) == Int:
-                print(this+i)
+                print(this+str(i))
             else:
-                print(thisnot+i)
+                print(thisnot+str(i))
         return Int
     printList(Int)
     yes = True
     while yes == True:
         ip = input()
-        if ip == "":
+        if ip in HiddenList:
+            return ip,ip
+        elif ip == "":
             break
         for k in ip:
             if k in nexti:
@@ -222,10 +287,11 @@ def chooseFromList(Import):
     return List[Int],Int
 
 # **chooseFromDictionary** allows you to print and select from items in a given
-# dictionary by entering the key or moving the selector up and down with the plus
-# and minus signs, or with brackets pointing left (backward) or right (forward),
-# also multiple in one input. Confirm every input with the Enter key.
-# The function requires one list of three or four elements:
+# dictionary by entering (a fragment of) the key or moving the selector up and
+# down with the plus and minus signs, or with brackets pointing left (backward)
+# or right (forward), also multiple in one input. Confirm every input with the
+# Enter key.
+# The function requires one list of three, four or five elements:
 # the Dictionary:     the given dictionary with the keys and values to choose
 #                     from.
 # the Showing option: The line with the indicator is always printed, plus:
@@ -235,17 +301,43 @@ def chooseFromList(Import):
 #                     2: print all lines, including the connector and the empty
 #                     value
 #                     3: print only the lines with an empty value
-# the Default Option: an existing key in the dictionary
-# Optional:           a customized indicator: Can contain a colour code (don't
+#                     the Default Option: an existing key in the dictionary
+# Optional1:          a customized indicator: Can contain a colour code (don't
 #                     forget to close it with "\033[0m"!). Default string length
 #                     is 3, but you can also change the length to make it stand
 #                     out. If it is "O", the first three characters of the chosen
 #                     key are used.
-# It returns the selected value and its key.
+# Optional2:          A list with valid options that are not shown in the
+#                     Dictionary. ATTENTION: It returns only the chosen option,
+#                     twice!
+# It returns the selected value and its key, except when an item from HiddenList
+# is chosen.
 def chooseFromDictionary(Import):
     Dictionary =    Import[0]
     ShowEmpty =     Import[1]
     DefaultOption = Import[2]
+    try:
+        if type(Import[3]) == str:
+            if Import[3].upper() == "O":
+                this = "{:^3}".format(Key)[:3]
+            else:
+                this = Import[3]
+            try:
+                HiddenList = Import[4]
+            except:
+                HiddenList = []
+        elif type(Import[3]) == list:
+            this = "-> "
+            HiddenList = Import[3]
+        else:
+            this = "-> "
+            try:
+                HiddenList = Import[4]
+            except:
+                HiddenList = []
+    except:
+        this = "-> "
+        HiddenList = []
     nexti = [")","}","]",">","+"]
     previ = ["(","{","[","<","-"]
     thisnot = "   "
@@ -255,26 +347,12 @@ def chooseFromDictionary(Import):
     JList = []
     for i,j in Dictionary.items():
         IList.append(i)
-        if j == None:
-            JList.append("(None)")
-        elif j == True:
-            JList.append("(True)")
-        elif j == False:
-            JList.append("(False)")
-        else:
-            JList.append(j)
+        JList.append(j)
     def printDictionary(Key):
-        try:
-            if Import[3].upper() == "O":
-                this = "{:^3}".format(Key)[:3]
-            else:
-                this = str(Import[3])
-        except:
-            this = "-> "
-        StrList = []
+        KeysList = []
         for i in IList:
-            StrList.append(str(i))
-        maxlen = len(max(StrList,key = len))
+            KeysList.append(str(i))
+        maxlen = len(max(KeysList,key = len))
         for i in IList:
             if ShowEmpty == 0:
                 if i == Key:
@@ -288,7 +366,6 @@ def chooseFromDictionary(Import):
                     print(thisnot+("{:>%d}" % maxlen).format(i))
                 else:
                     print(thisnot+("{:>%d}" % maxlen).format(i)+connect+str(JList[IList.index(i)]))
-
             elif ShowEmpty == 2:
                 if i == Key:
                     print(this+("{:>%d}" % maxlen).format(i)+connect+str(JList[IList.index(i)]))
@@ -306,51 +383,59 @@ def chooseFromDictionary(Import):
     yes = True
     while yes == True:
         NewKey = input()
-        if NewKey in IList:
-            Key = NewKey
-        elif NewKey == "":
+        if NewKey == "":
             break
-        else:
-            for k in NewKey:
-                if k in nexti:
-                    Key = IList[(IList.index(Key) + 1) % len(Dictionary)]
-                elif k in previ:
-                    Key = IList[(IList.index(Key) - 1) % len(Dictionary)]
-                else:
-                    pass
+        elif NewKey in HiddenList:
+            return NewKey,NewKey
+        for j in IList:
+            if NewKey in j:
+                Key = j
+                break
+        for k in NewKey:
+            if k in nexti:
+                Key = IList[(IList.index(Key) + 1) % len(Dictionary)]
+            elif k in previ:
+                Key = IList[(IList.index(Key) - 1) % len(Dictionary)]
+            else:
+                pass
         printDictionary(Key)
     # 0 = item in Dictionary, 1 = Key of that item in Dictionary
     return Dictionary[Key],Key
 
-#ExampleList = [
-#        "Als",
-#        "Bij",
-#        "Catania",
-#        "De",
-#        "Etna",
-#        "Fakkelt"
-#        ]
-#ExampleKeysList = [
-#        "A",
-#        "B",
-#        "C",
-#        "D",
-#        "E",
-#        "F"
-#        ] 
-#ExampleDictionary = {
-#        "A": "Als",
-#        "B": None,
-#        "C": "",
-#        "D": "De",
-#        "E": "Etna",
-#        "F": ""
-#        }
-#what, where = chooseFromNumberedList([ExampleList, "D", 1, 2])
-#print(what, where)
-#what, where = chooseFromKeysList([ExampleList, ExampleKeysList, "U", "c"])
-#print(what, where)
-#what, where = chooseFromList([ExampleList, 0, "---"])
-#print(what, where)
-#what, where = chooseFromDictionary([ExampleDictionary, 3, "B", "\033[32m"+"+- "+"\033[0m"])
-#print(what, where)
+ExampleList = [
+        "Als",
+        2,
+        "Catania",
+        None,
+        "Etna",
+        "Fakkelt"
+        ]
+ExampleKeysList = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F"
+        ] 
+ExampleDictionary = {
+        "A": "Als",
+        "B": None,
+        "C": "",
+        "D": True,
+        "E": "Etna",
+        "F": "Fakkelt"
+        }
+ExampleHiddenList = [
+        ":q",
+        ":u",
+        ":w"
+        ]
+what, where = chooseFromNumberedList([ExampleList, "D", 0, 2])
+print(what, where)
+what, where = chooseFromKeysList([ExampleList, ExampleKeysList, "U", "c"])
+print(what, where)
+what, where = chooseFromList([ExampleList, 0, "---"])
+print(what, where)
+what, where = chooseFromDictionary([ExampleDictionary, 3, "B", "\033[32m"+">- "+"\033[0m",ExampleHiddenList])
+print(what, where)
